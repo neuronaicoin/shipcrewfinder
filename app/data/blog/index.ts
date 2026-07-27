5997229b462e3aae81e7d3ac71b77882c5187c6b
NEURON AI <neuronaicoin@gmail.com>
2026-07-27 09:35:28 +0300

    Import crew retention article into blog index

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -1,5 +1,6 @@
 import type { BlogIndexEntry, BlogPost } from "./types";
 // Her yeni yazıyı buraya import et + posts dizisine ekle
+import crewRetentionWhySeafarersDontReturn from "./crew-retention-why-seafarers-dont-return";
 import howToFindQualifiedShipCrew from "./how-to-find-qualified-ship-crew-hiring-guide";
 import fakeSeafarerJobOffersScams from "./fake-seafarer-job-offers-recruitment-scams";
 import howToReadSeaRedFlags from "./how-to-read-seafarer-employment-agreement-red-flags";
@@ -21,6 +22,7 @@ import howToBecomeYachtCrew from "./how-to-become-yacht-crew";
 import tankerVsCargoShipJobs from "./tanker-vs-cargo-ship-jobs";
 // Tüm yazıların tam içeriği (tek yazı sayfası bundan okur)
 export const allPosts: BlogPost[] = [
+  crewRetentionWhySeafarersDontReturn,
   howToFindQualifiedShipCrew,
   fakeSeafarerJobOffersScams,
   howToReadSeaRedFlags,
