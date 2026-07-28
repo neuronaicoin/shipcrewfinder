92551bb6ddb6ab73525c287a81880492acd92d0e
NEURON AI <neuronaicoin@gmail.com>
2026-07-28 13:47:10 +0300

    Add STCW certificate expiry renewal checklist post

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -1,5 +1,6 @@
 import type { BlogIndexEntry, BlogPost } from "./types";
 // Her yeni yazıyı buraya import et + posts dizisine ekle
+import stcwCertificateExpiryRenewalChecklist from "./stcw-certificate-expiry-renewal-checklist";
 import chiefEngineerVsMasterSalary from "./chief-engineer-vs-master-salary";
 import howToGetSeafarerJobWithoutAgency from "./how-to-get-seafarer-job-without-agency";
 import seafarerSalaryGuide2026RankVesselType from "./seafarer-salary-guide-2026-rank-vessel-type";
@@ -26,6 +27,7 @@ import howToBecomeYachtCrew from "./how-to-become-yacht-crew";
 import tankerVsCargoShipJobs from "./tanker-vs-cargo-ship-jobs";
 // Tüm yazıların tam içeriği (tek yazı sayfası bundan okur)
 export const allPosts: BlogPost[] = [
+  stcwCertificateExpiryRenewalChecklist,
   chiefEngineerVsMasterSalary,
   howToGetSeafarerJobWithoutAgency,
   seafarerSalaryGuide2026RankVesselType,
