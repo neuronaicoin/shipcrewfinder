c83d3d51494b9b2d720218fe5d61d48c123903b1
NEURON AI <neuronaicoin@gmail.com>
2026-08-09 01:32:43 +0300

    Add crew rotation planning guide to blog index

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -1,5 +1,6 @@
 import type { BlogIndexEntry, BlogPost } from "./types";
 // Her yeni yazıyı buraya import et + posts dizisine ekle
+import crewRotationPlanningGuide from "./crew-rotation-planning-guide-avoid-sign-on-delays-2026";
 import tankerVettingInspectionSireCdiGuide from "./tanker-vetting-inspection-sire-cdi-guide";
 import stcwRefresherCoursesGuide2026 from "./stcw-refresher-courses-guide-2026";
 import panamaFlagEndorsementSeafarerApplicationGuide from "./panama-flag-endorsement-seafarer-application-guide";
@@ -43,6 +44,7 @@ import howToBecomeYachtCrew from "./how-to-become-yacht-crew";
 import tankerVsCargoShipJobs from "./tanker-vs-cargo-ship-jobs";
 // Tüm yazıların tam içeriği (tek yazı sayfası bundan okur)
 export const allPosts: BlogPost[] = [
+  crewRotationPlanningGuide,
   tankerVettingInspectionSireCdiGuide,
   stcwRefresherCoursesGuide2026,
   panamaFlagEndorsementSeafarerApplicationGuide,
