6e87df442163eafcf71667155a71abddf1177292
NEURON AI <neuronaicoin@gmail.com>
2026-08-11 09:22:46 +0300

    Add classificationSocietiesExplained to blog index

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -1,5 +1,6 @@
 import type { BlogIndexEntry, BlogPost } from "./types";
 // Her yeni yazıyı buraya import et + posts dizisine ekle
+import classificationSocietiesExplained from "./classification-societies-explained-seafarer-guide-2026";
 import realCostVacantPositionGuide from "./real-cost-vacant-crew-position-guide-2026";
 import piClubsExplainedGuide from "./pi-clubs-explained-seafarer-company-guide-2026";
 import turkishSeafarersGoingInternational from "./turkish-seafarers-going-international-2026";
@@ -73,6 +74,7 @@ import howToBecomeYachtCrew from "./how-to-become-yacht-crew";
 import tankerVsCargoShipJobs from "./tanker-vs-cargo-ship-jobs";
 // Tüm yazıların tam içeriği (tek yazı sayfası bundan okur)
 export const allPosts: BlogPost[] = [
+  classificationSocietiesExplained,
   realCostVacantPositionGuide,
   piClubsExplainedGuide,
   turkishSeafarersGoingInternational,
