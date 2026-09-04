2d045e36fc6787b921b23f72472ac6b044595461
NEURON AI <neuronaicoin@gmail.com>
2026-09-04 18:26:09 +0300

    Add howToGetPromotedFasterGuide2026 import

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -98,6 +98,7 @@ import multiRankJobSearchGuide2026 from "./multi-rank-job-search-strategy-seafar
 import verificationGapGuide2026 from "./verification-gap-why-companies-ignore-unverified-cvs-2026";
 import redFlagsHiringCrewGuide2026 from "./red-flags-hiring-crew-warning-signs-employers-2026";
 import starlinkSatelliteInternetGuide2026 from "./starlink-satellite-internet-at-sea-guide-crew-2026";
+import howToGetPromotedFasterGuide2026 from "./how-to-get-promoted-faster-seafarer-career-2026";
 import agencyFeesVsDirectGuide2026 from "./recruitment-agency-fees-vs-direct-hiring-cost-comparison-2026";
 import verifyCertificatesSeaTimeGuide2026 from "./how-to-verify-seafarer-certificates-sea-time-2026";
 import crewPipelineWithoutAgencyGuide2026 from "./building-reliable-crew-pipeline-without-agency-2026";
@@ -127,6 +128,7 @@ export const allPosts: BlogPost[] = [
   verificationGapGuide2026,
   redFlagsHiringCrewGuide2026,
   starlinkSatelliteInternetGuide2026,
+  howToGetPromotedFasterGuide2026,
   agencyFeesVsDirectGuide2026,
   verifyCertificatesSeaTimeGuide2026,
   crewPipelineWithoutAgencyGuide2026,
